a9294da9798b87dd85dc79032b40340b156de2b5
jordan <jbs.io@protonmail.com>
2023-05-17 16:20:18 +0200

    TP2

--- src/classes/Person.ts
+++ src/classes/Person.ts
@@ -3,18 +3,27 @@ import PersonException from "./PersonException";
 export class Person {
     constructor(public firstname: string, public lastname: string) {}
 
-    static generateGroups(persons: Person[], groupCount: number): Person[][] {
-        if (persons.length % groupCount !== 0) {
-          throw new PersonException('Le nombre de personnes ne peut pas être réparti également dans les groupes.');
+    static generateGroups(persons: Person[], memberCount: number): Person[][] {
+        if (memberCount < 2) {
+          throw new PersonException('Le nombre de membres doit être supérieur ou égal à 2.');
+        }
+
+        if (memberCount > persons.length) {
+          throw new PersonException('Le nombre de membres ne peut pas être supérieur à l\'effectif total des personnes.');
+        }
+
+        if (persons.length % memberCount !== 0) {
+          throw new PersonException('Le nombre de personnes ne peut pas être réparti uniformément entre les membres.');
         }
     
         const clonedPersons = [...persons];
     
         const groups: Person[][] = [];
-        const groupSize = clonedPersons.length / groupCount;
+        const groupSize = clonedPersons.length / memberCount;
     
-        for (let i = 0; i < groupCount; i++) {
+        for (let i = 0; i < memberCount; i++) {
           const group: Person[] = [];
+
           for (let j = 0; j < groupSize; j++) {
             const randomIndex = Math.floor(Math.random() * clonedPersons.length);
             const person = clonedPersons.splice(randomIndex, 1)[0];
--- tests/person.test.ts
+++ tests/person.test.ts
@@ -17,18 +17,50 @@ describe("Person", () => {
         persons.push(person);
       }
 
-      const groupCount = 5;
-      const groups = Person.generateGroups(persons, groupCount);
+      const memberCount = 4;
+      const groups = Person.generateGroups(persons, memberCount);
 
-      expect(groups.length).toBe(groupCount);
+      expect(groups.length).toBe(memberCount);
 
-      const groupSize = persons.length / groupCount;
       groups.forEach((group) => {
-        expect(group.length).toBe(groupSize);
+        expect(group.length).toBe(persons.length / memberCount);
       });
     });
 
-    test("devrait générer une erreur s'il n'est pas possible de répartir uniformément les personnes dans les groupes", () => {
+    test("devrait générer une erreur lorsque le nombre de membres est inférieur à 2", () => {
+      const persons: Person[] = [];
+      for (let i = 0; i < 20; i++) {
+        const firstname = faker.person.firstName();
+        const lastname = faker.person.lastName();
+        const person = new Person(firstname, lastname);
+        persons.push(person);
+      }
+
+      const memberCount = 1;
+
+      expect(() => {
+        Person.generateGroups(persons, memberCount);
+      }).toThrowError(new PersonException("Le nombre de membres doit être supérieur ou égal à 2."));
+    });
+
+
+    test("devrait générer une erreur lorsque le nombre de membres est supérieur à l'effectif total des personnes", () => {
+      const persons: Person[] = [];
+      for (let i = 0; i < 20; i++) {
+        const firstname = faker.person.firstName();
+        const lastname = faker.person.lastName();
+        const person = new Person(firstname, lastname);
+        persons.push(person);
+      }
+
+      const memberCount = 25;
+
+      expect(() => {
+        Person.generateGroups(persons, memberCount);
+      }).toThrowError(new PersonException("Le nombre de membres ne peut pas être supérieur à l'effectif total des personnes."));
+    });
+
+    test("devrait générer une erreur lorsque le nombre de personnes ne peut pas être réparti uniformément entre les membres", () => {
       const persons: Person[] = [];
       for (let i = 0; i < 19; i++) {
         const firstname = faker.person.firstName();
@@ -37,11 +69,11 @@ describe("Person", () => {
         persons.push(person);
       }
 
-      const groupCount = 5;
+      const memberCount = 5;
 
       expect(() => {
-        Person.generateGroups(persons, groupCount);
-      }).toThrowError(new PersonException("Le nombre de personnes ne peut pas être réparti également dans les groupes."));
+        Person.generateGroups(persons, memberCount);
+      }).toThrowError(new PersonException("Le nombre de personnes ne peut pas être réparti uniformément entre les membres."));
     });
   });
 });

19ac74046c22afd442bd7129f9492153ff2ff475
jordan <jbs.io@protonmail.com>
2023-05-17 16:30:21 +0200

    TP3

--- src/classes/Person.ts
+++ src/classes/Person.ts
@@ -1,37 +1,89 @@
 import PersonException from "./PersonException";
 
 export class Person {
-    constructor(public firstname: string, public lastname: string) {}
+  constructor(public firstname: string, public lastname: string) {}
 
-    static generateGroups(persons: Person[], memberCount: number): Person[][] {
-        if (memberCount < 2) {
-          throw new PersonException('Le nombre de membres doit être supérieur ou égal à 2.');
-        }
+  static generateGroups(persons: Person[], groupCount: number): Person[][] {
+    if (persons.length % groupCount !== 0) {
+      throw new PersonException("Le nombre de personnes ne peut pas être réparti également dans les groupes.");
+    }
 
-        if (memberCount > persons.length) {
-          throw new PersonException('Le nombre de membres ne peut pas être supérieur à l\'effectif total des personnes.');
-        }
+    const clonedPersons = [...persons];
 
-        if (persons.length % memberCount !== 0) {
-          throw new PersonException('Le nombre de personnes ne peut pas être réparti uniformément entre les membres.');
-        }
-    
-        const clonedPersons = [...persons];
-    
-        const groups: Person[][] = [];
-        const groupSize = clonedPersons.length / memberCount;
-    
-        for (let i = 0; i < memberCount; i++) {
-          const group: Person[] = [];
+    const groups: Person[][] = [];
+    const groupSize = clonedPersons.length / groupCount;
 
-          for (let j = 0; j < groupSize; j++) {
-            const randomIndex = Math.floor(Math.random() * clonedPersons.length);
-            const person = clonedPersons.splice(randomIndex, 1)[0];
-            group.push(person);
-          }
-          groups.push(group);
-        }
-    
-        return groups;
+    for (let i = 0; i < groupCount; i++) {
+      const group: Person[] = [];
+      for (let j = 0; j < groupSize; j++) {
+        const randomIndex = Math.floor(Math.random() * clonedPersons.length);
+        const person = clonedPersons.splice(randomIndex, 1)[0];
+        group.push(person);
       }
+      groups.push(group);
+    }
+
+    return groups;
+  }
+
+  static generateMembers(persons: Person[], memberCount: number): Person[][] {
+    if (memberCount < 2) {
+      throw new PersonException("Le nombre de membres doit être supérieur ou égal à 2.");
+    }
+
+    if (memberCount > persons.length) {
+      throw new PersonException("Le nombre de membres ne peut pas être supérieur à l'effectif total des personnes.");
+    }
+
+    if (persons.length % memberCount !== 0) {
+      throw new PersonException("Le nombre de personnes ne peut pas être réparti uniformément entre les membres.");
+    }
+
+    const clonedPersons = [...persons];
+
+    const groups: Person[][] = [];
+    const groupSize = clonedPersons.length / memberCount;
+
+    for (let i = 0; i < memberCount; i++) {
+      const group: Person[] = [];
+
+      for (let j = 0; j < groupSize; j++) {
+        const randomIndex = Math.floor(Math.random() * clonedPersons.length);
+        const person = clonedPersons.splice(randomIndex, 1)[0];
+        group.push(person);
+      }
+      groups.push(group);
+    }
+
+    return groups;
+  }
+
+  static generateMultipleOfEightGroups(persons: Person[]): Person[][] {
+    if (persons.length < 16) {
+      throw new PersonException("Le nombre de personnes doit être supérieur ou égal à 16 pour générer des groupes multiples de 8.");
+    }
+
+    if (persons.length % 8 !== 0) {
+      throw new PersonException("Le nombre de personnes ne permet pas d'obtenir des groupes multiples de 8.");
+    }
+
+    const clonedPersons = [...persons];
+
+    const groupCount = persons.length / 8;
+    const groups: Person[][] = [];
+
+    for (let i = 0; i < groupCount; i++) {
+      const group: Person[] = [];
+
+      for (let j = 0; j < 8; j++) {
+        const randomIndex = Math.floor(Math.random() * clonedPersons.length);
+        const person = clonedPersons.splice(randomIndex, 1)[0];
+        group.push(person);
+      }
+
+      groups.push(group);
+    }
+
+    return groups;
+  }
 }
--- tests/person.test.ts
+++ tests/person.test.ts
@@ -17,8 +17,46 @@ describe("Person", () => {
         persons.push(person);
       }
 
+      const groupCount = 5;
+      const groups = Person.generateGroups(persons, groupCount);
+
+      expect(groups.length).toBe(groupCount);
+
+      const groupSize = persons.length / groupCount;
+      groups.forEach((group) => {
+        expect(group.length).toBe(groupSize);
+      });
+    });
+
+    test("devrait générer une erreur s'il n'est pas possible de répartir uniformément les personnes dans les groupes", () => {
+      const persons: Person[] = [];
+      for (let i = 0; i < 19; i++) {
+        const firstname = faker.person.firstName();
+        const lastname = faker.person.lastName();
+        const person = new Person(firstname, lastname);
+        persons.push(person);
+      }
+
+      const groupCount = 5;
+
+      expect(() => {
+        Person.generateGroups(persons, groupCount);
+      }).toThrowError(new PersonException("Le nombre de personnes ne peut pas être réparti également dans les groupes."));
+    });
+  });
+
+  describe("generateMembers", () => {
+    test("devrait générer des groupes composés d'un nombre égal de personnes", () => {
+      const persons: Person[] = [];
+      for (let i = 0; i < 20; i++) {
+        const firstname = faker.person.firstName();
+        const lastname = faker.person.lastName();
+        const person = new Person(firstname, lastname);
+        persons.push(person);
+      }
+
       const memberCount = 4;
-      const groups = Person.generateGroups(persons, memberCount);
+      const groups = Person.generateMembers(persons, memberCount);
 
       expect(groups.length).toBe(memberCount);
 
@@ -39,10 +77,9 @@ describe("Person", () => {
       const memberCount = 1;
 
       expect(() => {
-        Person.generateGroups(persons, memberCount);
+        Person.generateMembers(persons, memberCount);
       }).toThrowError(new PersonException("Le nombre de membres doit être supérieur ou égal à 2."));
     });
-
 
     test("devrait générer une erreur lorsque le nombre de membres est supérieur à l'effectif total des personnes", () => {
       const persons: Person[] = [];
@@ -56,7 +93,7 @@ describe("Person", () => {
       const memberCount = 25;
 
       expect(() => {
-        Person.generateGroups(persons, memberCount);
+        Person.generateMembers(persons, memberCount);
       }).toThrowError(new PersonException("Le nombre de membres ne peut pas être supérieur à l'effectif total des personnes."));
     });
 
@@ -72,8 +109,56 @@ describe("Person", () => {
       const memberCount = 5;
 
       expect(() => {
-        Person.generateGroups(persons, memberCount);
+        Person.generateMembers(persons, memberCount);
       }).toThrowError(new PersonException("Le nombre de personnes ne peut pas être réparti uniformément entre les membres."));
+    });
+  });
+
+  describe("generateMultipleOfEightGroups", () => {
+    test("devrait générer des groupes multiples de 8", () => {
+      const persons: Person[] = [];
+      for (let i = 0; i < 32; i++) {
+        const firstname = faker.person.firstName();
+        const lastname = faker.person.lastName();
+        const person = new Person(firstname, lastname);
+        persons.push(person);
+      }
+
+      const groups = Person.generateMultipleOfEightGroups(persons);
+
+      expect(groups.length).toBe(4);
+
+      groups.forEach((group) => {
+        expect(group.length).toBe(8);
+      });
+    });
+
+    test("devrait générer une erreur si le nombre de personnes est inférieur à 16", () => {
+      const persons: Person[] = [];
+      for (let i = 0; i < 15; i++) {
+        const firstname = faker.person.firstName();
+        const lastname = faker.person.lastName();
+        const person = new Person(firstname, lastname);
+        persons.push(person);
+      }
+
+      expect(() => {
+        Person.generateMultipleOfEightGroups(persons);
+      }).toThrowError(new PersonException('Le nombre de personnes doit être supérieur ou égal à 16 pour générer des groupes multiples de 8.'));
+    });
+
+    test("devrait générer une erreur si le nombre de personnes ne permet pas d'obtenir des groupes multiples de 8", () => {
+      const persons: Person[] = [];
+      for (let i = 0; i < 18; i++) {
+        const firstname = faker.person.firstName();
+        const lastname = faker.person.lastName();
+        const person = new Person(firstname, lastname);
+        persons.push(person);
+      }
+
+      expect(() => {
+        Person.generateMultipleOfEightGroups(persons);
+      }).toThrowError(new PersonException('Le nombre de personnes ne permet pas d\'obtenir des groupes multiples de 8.'));
     });
   });
 });
